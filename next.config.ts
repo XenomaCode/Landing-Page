270b9bc9f709b4774e847b8af57f352970c95cb6
Moises Hernandez Alvarado <MoisesHernandezAlvarado@hotmail.com>
2025-02-28 15:15:18 -0600

    mi cagadero

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // Asegúrate de que los archivos estáticos se copien correctamente
+  images: {
+    domains: [],
+  },
+  // Configuración para archivos estáticos
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
