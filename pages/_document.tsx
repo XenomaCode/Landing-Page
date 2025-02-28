9a95027cc16e8e8389899fd3950de0d9b8ba994f
Moises Hernandez Alvarado <MoisesHernandezAlvarado@hotmail.com>
2025-02-28 15:22:34 -0600

    moy

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/xenomaicono.svg" type="image/svg+xml" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+} 
