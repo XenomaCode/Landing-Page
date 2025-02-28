464ce737861c4f27692a97c6d8284ea67babb381
Moises Hernandez Alvarado <MoisesHernandezAlvarado@hotmail.com>
2025-02-28 10:57:49 -0600

    [add] iconos

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-    return <div>Bienvenido a XENOMA CODE</div>;
-  }
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/xenomaicono.svg" /> {/* Cambia "/favicon.ico" por la ruta de tu archivo */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
