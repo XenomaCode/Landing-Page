acdf5cfc910fc0c3df62b59278baf507dd1c1b3e
Moises Hernandez Alvarado <MoisesHernandezAlvarado@hotmail.com>
2025-02-28 11:19:51 -0600

    [fix] cagadero de oziel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
@@ -10,17 +11,24 @@ import '../styles/globals.css';
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen">
-      <AnimatedBackground />
-      <div className="relative z-10">
-        <Header />
-        <Hero />
-        <Services />
-        <WhyChooseUs />
-        <OurWork />
-        <OurTeam />
-        <Footer />
+    <>
+      <Head>
+        <title>Xenoma Code</title>
+        <link rel="icon" href="/xenomaicono.svg" />
+        <meta name="description" content="Desarrollamos soluciones innovadoras para impulsar tu negocio al siguiente nivel" />
+      </Head>
+      <div className="relative min-h-screen">
+        <AnimatedBackground />
+        <div className="relative z-10">
+          <Header />
+          <Hero />
+          <Services />
+          <WhyChooseUs />
+          <OurWork />
+          <OurTeam />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
