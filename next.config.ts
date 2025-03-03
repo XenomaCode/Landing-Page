9a860b63001c89dbb4c8f7056e1db38da773b0bd
Oziel Lemus <114196082+OzielLM@users.noreply.github.com>
2025-03-02 18:14:48 -0600

    [fix] Arreglo del pedo de Moy
    Se arreglo el favicon para la landing page

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // Asegúrate de que los archivos estáticos se copien correctamente
-  images: {
-    domains: [],
-  },
-  // Configuración para archivos estáticos
-  webpack: (config) => {
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
